6e54cc2bbe51c6a38be2723a5110d72a0229b842
Efejjota <efejjota@gmail.com>
2021-02-26 22:53:01 +0000

    Allow connections from localhost by default.
    - This also allows using this starter without
    defining anything in application.properties

--- src/main/java/org/joulesjs/JoulesJsProperties.java
+++ src/main/java/org/joulesjs/JoulesJsProperties.java
@@ -1,13 +1,12 @@
 package org.joulesjs;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "joulesjs")
 public class JoulesJsProperties {
-  private List<String> allowedIps = Collections.emptyList();
+  private List<String> allowedIps = List.of("127.0.0.1", "0:0:0:0:0:0:0:1");
 
   public List<String> getAllowedIps() {
     return allowedIps;
